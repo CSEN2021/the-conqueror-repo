c1393a4caff1ccd9270a3f506da7392468641361
Yousef Allam <yousef.allam221b@gmail.com>
2021-04-24 22:57:16 +0200

    getters and setters for buildings

--- the conqueror project/src/buildings/MilitaryBuilding.java
+++ the conqueror project/src/buildings/MilitaryBuilding.java
@@ -4,12 +4,32 @@ public class MilitaryBuilding extends Building{
 
 	private int recruitmentCost;
 	private int currentRecruit;
-	final private int maxRecruit = 3;
+	private int maxRecruit = 3;
 	
 	public MilitaryBuilding(int cost, int upgradeCost,int recruitmentCost)
 	{
 		super(cost, upgradeCost);
 		this.recruitmentCost = recruitmentCost;
+	}
+
+	public int getRecruitmentCost() {
+		return recruitmentCost;
+	}
+
+	public void setRecruitmentCost(int recruitmentCost) {
+		this.recruitmentCost = recruitmentCost;
+	}
+
+	public int getCurrentRecruit() {
+		return currentRecruit;
+	}
+
+	public void setCurrentRecruit(int currentRecruit) {
+		this.currentRecruit = currentRecruit;
+	}
+
+	public int getMaxRecruit() {
+		return maxRecruit;
 	}
 	
 }
