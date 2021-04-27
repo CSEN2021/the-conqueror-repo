6e23de2bca4e34d8bc797eb154c67d65810eabb5
wagdy <wagdy@192.168.1.9>
2021-04-27 20:05:36 +0200

    finished units section without the csv part(values)

--- the conqueror project/src/units/Unit.java
+++ the conqueror project/src/units/Unit.java
@@ -2,11 +2,55 @@ package units;
 
 public abstract class Unit {
 	
-	int level;
+	private int level;
+	private int maxSoldierCount;
+	private int currentSoldierCount;
+	private double idleUpkeep;
+	private double marchingUpkeep;
+	private double siegeUpkeep;
 	
-	
-	public Unit() {
-		
+	public Unit(int level, int maxSoldierCount, double idleUpkeep, double marchingUpkeep,double siegeUpkeep) {
+		this.level = level;
+		this.maxSoldierCount = maxSoldierCount;
+		this.idleUpkeep = idleUpkeep;
+		this.marchingUpkeep = marchingUpkeep;
+		this.siegeUpkeep = siegeUpkeep;
 	}
 
+	public int getCurrentSoldierCount() {
+		return currentSoldierCount;
+	}
+
+
+	public void setCurrentSoldierCount(int currentSoldierCount) {
+		this.currentSoldierCount = currentSoldierCount;
+	}
+
+
+	public int getLevel() {
+		return level;
+	}
+
+
+	public int getMaxSoldierCount() {
+		return maxSoldierCount;
+	}
+
+
+	public double getIdleUpkeep() {
+		return idleUpkeep;
+	}
+
+
+	public double getMarchingUpkeep() {
+		return marchingUpkeep;
+	}
+
+
+	public double getSiegeUpkeep() {
+		return siegeUpkeep;
+	}
+
+
+	
 }
