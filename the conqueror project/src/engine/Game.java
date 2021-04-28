585562db92da70d2daa3ad54358cf9d8e403c075
MohamedGallab <moknaruto@gmail.com>
2021-04-28 20:41:25 +0200

    loadCitiesAndDistances implemented

--- the conqueror project/src/engine/Game.java
+++ the conqueror project/src/engine/Game.java
@@ -3,41 +3,42 @@ package engine;
 import java.util.*;
 import buildings.*;
 import units.*;
-import java.io.IOException;    //new import for me
+import java.io.IOException; //new import for me
 
 public class Game {
-	
-	//instance variables
+
+	// instance variables
 	private Player player;
-	private ArrayList<City> availableCities = new ArrayList<>();	//read only
-	private ArrayList<Distance> distances = new ArrayList<>();	//read only
-	final private int maxTurnCount = 30;	//read only
+	private ArrayList<City> availableCities = new ArrayList<>(); // read only
+	private ArrayList<Distance> distances = new ArrayList<>(); // read only
+	final private int maxTurnCount = 30; // read only
 	private int currentTurnCount = 1;
-	
-	//constructor
-	public Game(String playerName,String playerCity) throws IOException {
-		
+
+	// constructor
+	public Game(String playerName, String playerCity) throws IOException {
+
 		this.player = new Player(playerName);
-		
-		//fill the lists
+
+		// fill the lists
 		loadCitiesAndDistances();
-		
-		//incomplete, check milestone description
-		//"Carefully think about how will you initialize the army of the defending cities"
+
+		// incomplete, check milestone description
+		// "Carefully think about how will you initialize the army of the defending
+		// cities"
 	}
-	
-	//methods
-	
-	//initializes the defending army of the defending cities
-	public void loadArmy(String cityName,String path) throws IOException {
-		
+
+	// methods
+
+	// initializes the defending army of the defending cities
+	public void loadArmy(String cityName, String path) throws IOException {
+
 	}
-	
+
 	private void loadCitiesAndDistances() throws IOException {
 		ReadCSV.readFile("distances", availableCities, distances);
 	}
-	
-	//getters and setters
+
+	// getters and setters
 	public Player getPlayer() {
 		return player;
 	}
@@ -65,5 +66,5 @@ public class Game {
 	public int getMaxTurnCount() {
 		return maxTurnCount;
 	}
-	
+
 }
--- the conqueror project/src/engine/ReadCSV.java
+++ the conqueror project/src/engine/ReadCSV.java
@@ -7,25 +7,50 @@ import units.*;
 public class ReadCSV {
 
 	// Reads into ArrayList<City> availableCities & ArrayList<Distance> distances
-	public static void readFile(String path, ArrayList<City> availableCities, ArrayList<Distance> distances)
-			throws IOException {
+	public static void readFile(String path, ArrayList<City> availableCities, ArrayList<Distance> distances) throws IOException 
+	{
 		String currentLine = "";
 		FileReader fileReader = new FileReader(path); // open file "path"
 		BufferedReader br = new BufferedReader(fileReader); // read from it
 
-		while ((currentLine = br.readLine()) != null) {
+		while ((currentLine = br.readLine()) != null) 
+		{
 
 			String[] result = currentLine.split(","); // save to result
 
 			distances.add(new Distance(result[0], result[1], Integer.parseInt(result[2]))); // fill distances
 
 			// check if city already added if not then add it
-			//needs to be rewritten because it obviously doesn't contain something written with keyword 'new'
-			if (!availableCities.contains(new City(result[0]))) {
+			if (availableCities.isEmpty()) 
+			{
 				availableCities.add(new City(result[0]));
-			}
-			if (!availableCities.contains(new City(result[1]))) {
 				availableCities.add(new City(result[1]));
+			} 
+			else 
+			{
+				// iterate to check if city is already in the list
+				boolean flag0 = true;
+				boolean flag1 = true;
+				for (int i = 0; i < availableCities.size(); i++) 
+				{
+					// set flag to false if list already has the cities
+					if (availableCities.get(i).getName().equals(result[0])) 
+					{
+						flag0 = false;
+					}
+					if (availableCities.get(i).getName().equals(result[1])) 
+					{
+						flag1 = false;
+					}
+				}
+				if (flag0) 
+				{
+					availableCities.add(new City(result[0]));
+				}
+				if (flag1) 
+				{
+					availableCities.add(new City(result[1]));
+				}
 			}
 		}
 	}
@@ -36,25 +61,26 @@ public class ReadCSV {
 		FileReader fileReader = new FileReader(path); // open file "path"
 		BufferedReader br = new BufferedReader(fileReader); // read from it
 
-		ArrayList<Unit> units = city.getDefendingArmy().getUnits();	// units of the army of the city
-		while ((currentLine = br.readLine()) != null) {
+		ArrayList<Unit> units = city.getDefendingArmy().getUnits(); // units of the army of the city
+		while ((currentLine = br.readLine()) != null) 
+		{
 
 			String[] result = currentLine.split(","); // save to result
-			if(units.contains(result))
-			
-			// adding units
-			switch (result[0]) {
-			// initialize units with corresponding level
-			case "Archer":
-				units.add(Archer.archer(result[1]));
-				break;
-			case "Infantry":
-				units.add(Infantry.infantry(result[1]));
-				break;
-			case "Cavalry":
-				units.add(Cavalry.cavalry(result[1]));
-				break;
-			}
+
+				// adding units
+				switch (result[0]) 
+				{
+				// initialize units with corresponding level
+				case "Archer":
+					units.add(Archer.archer(result[1]));
+					break;
+				case "Infantry":
+					units.add(Infantry.infantry(result[1]));
+					break;
+				case "Cavalry":
+					units.add(Cavalry.cavalry(result[1]));
+					break;
+				}
 
 		}
 	}
