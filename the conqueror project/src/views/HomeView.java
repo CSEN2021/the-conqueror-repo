2376aab986de176c69c8dac4aaedb685445d684c
Yousef Allam <yousef.allam221b@gmail.com>
2021-06-24 00:21:31 +0200

    grid layout homepage

--- the conqueror project/src/views/HomeView.java
+++ the conqueror project/src/views/HomeView.java
@@ -7,9 +7,14 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.Image;
 
+import javax.swing.AbstractButton;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -23,31 +28,48 @@ public class HomeView extends JFrame{
 		this.setSize(1280, 720);
 		Label welcome = new Label("Welcome");
 		JButton startGame = new JButton("Start Game");
-		JTextField playerName = new JTextField("Enter your name");
+		JTextField playerName = new JTextField();
+		JLabel pic = new JLabel();
+		ImageIcon image = new ImageIcon("Images/480378.jpg");
 		
-		JPanel topPanel = new JPanel();
-		JPanel midPanel = new JPanel();
-		
-		
-		this.add(topPanel,BorderLayout.NORTH);
-		this.add(midPanel,BorderLayout.CENTER);
-		
-		topPanel.setPreferredSize(new Dimension(100,360));
-		topPanel.setBackground(Color.GRAY);
-		
-		
+		this.add(pic);
         playerName.setSize(new Dimension(5,10));
-		midPanel.setLayout(new BorderLayout());
-        
-        midPanel.add(playerName,BorderLayout.NORTH);
-		midPanel.add(startGame,BorderLayout.CENTER);
-		topPanel.add(welcome,BorderLayout.CENTER);
-		
+        startGame.setSize(50,100);
+		this.setLayout(new GridLayout(7,7));
+		for (int i = 0; i < 49; i++)
+		{
+			if (i == 17)
+			{
+				JPanel panel = new JPanel();
+				
+				panel.add(welcome,BorderLayout.CENTER);
+				this.add(panel);
+			}
+			else if (i == 24)
+			{
+				this.add(playerName,BorderLayout.CENTER);
+			}
+			else if (i == 31)
+			{
+				this.add(startGame,BorderLayout.CENTER);
+			}
+			else
+			{
+				Label panel = new Label();
+				this.add(panel,BorderLayout.CENTER);
+			}
+		}
+				
+		this.pack();
 		this.setTitle("Homepage");
 		this.setResizable(false);
 		this.setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.validate();
 		this.repaint();
+	}
+	
+	public static void main(String[] args) {
+		new HomeView();
 	}
 }
