149a384f562c677f3470ac3dc5f550613e9f4f5c
Mohamed Gallab <moknaruto@gmail.com>
2021-06-24 00:28:31 +0200

    start menu

--- the conqueror project/src/views/HomeView.java
+++ the conqueror project/src/views/HomeView.java
@@ -7,11 +7,13 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.GridLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.border.Border;
 
 
 
@@ -21,29 +23,36 @@ public class HomeView extends JFrame{
 	{
 		new JFrame();
 		this.setSize(1280, 720);
+		
 		Label welcome = new Label("Welcome");
 		JButton startGame = new JButton("Start Game");
 		JTextField playerName = new JTextField("Enter your name");
 		
 		JPanel topPanel = new JPanel();
 		JPanel midPanel = new JPanel();
+		JPanel bottomPanel = new JPanel();
+		JPanel filler = new JPanel();
+		topPanel.setLayout(new BorderLayout());
+		JPanel welcomePanel = new JPanel();
+		JTextField enterYourNameField = new JTextField("Please enter your name");
+		midPanel.setBackground(Color.CYAN);
+		welcomePanel.setBackground(Color.red);
+		bottomPanel.setPreferredSize(new Dimension(0,330));
 		
+		topPanel.setPreferredSize(new Dimension(0,320));
+		this.add(topPanel, BorderLayout.NORTH);
+		this.add(midPanel, BorderLayout.CENTER);
+		this.add(bottomPanel, BorderLayout.SOUTH);
+
 		
-		this.add(topPanel,BorderLayout.NORTH);
-		this.add(midPanel,BorderLayout.CENTER);
+		topPanel.add(filler,BorderLayout.CENTER);
+		topPanel.add(welcomePanel,BorderLayout.SOUTH);
+		welcomePanel.add(welcome,BorderLayout.CENTER);
+		midPanel.add(enterYourNameField);
+		bottomPanel.add(startGame,BorderLayout.CENTER);
 		
-		topPanel.setPreferredSize(new Dimension(100,360));
-		topPanel.setBackground(Color.GRAY);
-		
-		
-        playerName.setSize(new Dimension(5,10));
-		midPanel.setLayout(new BorderLayout());
-        
-        midPanel.add(playerName,BorderLayout.NORTH);
-		midPanel.add(startGame,BorderLayout.CENTER);
-		topPanel.add(welcome,BorderLayout.CENTER);
-		
-		this.setTitle("Homepage");
+       
+		this.setTitle("The Conquerer");
 		this.setResizable(false);
 		this.setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
