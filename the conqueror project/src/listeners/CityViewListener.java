f65026de5497b1f38f9f7f55cdd71f59a8789b65
Yousef Allam <yousef.allam221b@gmail.com>
2021-06-24 23:52:55 +0200

    mine

--- the conqueror project/src/listeners/CityViewListener.java
+++ the conqueror project/src/listeners/CityViewListener.java
@@ -2,5 +2,12 @@ package listeners;
 
 public interface CityViewListener
 {
+	public void onBuild();
+	public void onUpgrade();
+	public void onRecruit();
+	
+	public void onSelectBuilding();
+	public void onSelectLevel();
+	public void onSelectUnit();
 	
 }
